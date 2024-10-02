cb9b1853fef338ff9419f9c68a788bd3771702c7
akashdeep023 <ad3500476@gmail.com>
2024-10-03 04:23:56 +0530

    Add Data

---
+++ src/utils/constants/constant.js
@@ -0,0 +1,141 @@
+import { FaceControls } from "@react-three/drei";
+
+export const planetsData = [
+	{
+		planet_name: "Proxima Centauri b",
+		image: "/planets/Artist’s_impression_of_Proxima_Centauri_b_shown_hypothetically_as_an_arid_rocky_super-earth.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Proxima Centauri",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "May have a thin atmosphere, no confirmed detection yet",
+		},
+		distance_from_earth_light_years: 4.24,
+	},
+	{
+		planet_name: "Barnard's Star b",
+		image: "/planets/BarnardsStarb (1).jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Barnard's Star",
+		habitable: false,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Very cold, unlikely to support life",
+		},
+		distance_from_earth_light_years: 5.96,
+	},
+	{
+		planet_name: "Ross 128 b",
+		image: "/planets/image_6188-Ross-128b.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Ross 128",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Potentially temperate, but atmosphere not yet determined",
+		},
+		distance_from_earth_light_years: 11.03,
+	},
+	{
+		planet_name: "Luyten b",
+		image: "/planets/Luyten b.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Luyten's Star",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "In the habitable zone, but atmospheric composition unknown",
+		},
+		distance_from_earth_light_years: 12.2,
+	},
+	{
+		planet_name: "Teegarden b",
+		image: "/planets/Teegardens_Star_B_Artistic_depiction.png",
+		galaxy_name: "Milky Way",
+		star_name: "Teegarden's Star",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Likely temperate, atmosphere details unknown",
+		},
+		distance_from_earth_light_years: 12.5,
+	},
+	{
+		planet_name: "Gliese 1061 d",
+		image: "/planets/Gliese 1061 d.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Gliese 1061",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Potentially habitable but unconfirmed",
+		},
+		distance_from_earth_light_years: 12.0,
+	},
+	{
+		planet_name: "YZ Ceti d",
+		image: "/planets/YZ ceti d.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "YZ Ceti",
+		habitable: false,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Extreme radiation levels, unlikely to support life",
+		},
+		distance_from_earth_light_years: 12.1,
+	},
+	{
+		planet_name: "Wolf 1061 c",
+		image: "/planets/wolf 1061 c.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Wolf 1061",
+		habitable: false,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Inhabitable zone but likely too hot to support life",
+		},
+		distance_from_earth_light_years: 13.8,
+	},
+	{
+		planet_name: "Kapteyn b",
+		image: "/planets/KapteynB.jpg",
+		galaxy_name: "Milky Way",
+		star_name: "Kapteyn's Star",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "Potentially habitable, atmosphere not confirmed",
+		},
+		distance_from_earth_light_years: 12.8,
+	},
+	{
+		planet_name: "TRAPPIST-1 d",
+		image: "/planets/TRAPPIST-1d_artist_impression_2018.png",
+		galaxy_name: "Milky Way",
+		star_name: "TRAPPIST-1",
+		habitable: true,
+		atmospheric_conditions: {
+			composition: "unknown",
+			notes: "May have a rocky surface, atmospheric conditions not fully confirmed",
+		},
+		distance_from_earth_light_years: 39.6,
+	},
+];
+
+export const aboutDetails = {
+	description: [
+		"Welcome to our exoplanet exploration hub, a platform designed to ignite curiosity and foster learning about the vast universe beyond our solar system. Our interactive chatbot allows students to ask questions and dive deep into the fascinating world of exoplanets, with detailed information and striking images of these distant worlds. But that's just the beginning!",
+		"We're introducing a cutting-edge virtual reality (VR) experience, where students can immerse themselves in a 360° environment and explore exoplanets like never before. Accompanied by captivating audio descriptions, our VR simulations make learning about these extraordinary planets an unforgettable adventure.",
+		"Our goal is to make complex scientific concepts both accessible and engaging. Through the power of AI and immersive technology, we aim to transform the way students learn about space. Join us on this journey to the stars, and let your imagination soar beyond the limits of our solar system.",
+	],
+};
+export const contactDetails = {
+	mobile: ["(202) 358-0001 (Office)", "(202) 358-4338 (Fax)"],
+	address:
+		"Mary W. Jackson NASA Headquarters 300 E. Street SW, Suite 5R30 Washington, DC 20546",
+	facebook: "https://www.facebook.com/NASA",
+	instagram: "https://www.instagram.com/nasa/",
+	twitter: "https://x.com/NASA",
+	youtube: "https://www.youtube.com/@NASA",
+};
