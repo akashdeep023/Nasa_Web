80714862f93c27906b9b42c9805299b4597ea420
akashdeep023 <ad3500476@gmail.com>
2024-10-03 04:24:05 +0530

    Update

--- src/App.js
+++ src/App.js
@@ -7,9 +7,12 @@ import Login from "./components/page/Login";
 import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Loading from "./components/Loading";
-import PageNotFound from "./components/PageNotFound";
 import { ToastContainer } from "react-toastify";
 import "react-toastify/dist/ReactToastify.css";
+const PageNotFound = lazy(() => import("./components/PageNotFound"));
+const Planets = lazy(() => import("./components/page/Planets"));
+const Contact = lazy(() => import("./components/page/Contact"));
+const AboutUs = lazy(() => import("./components/page/AboutUs"));
 const Home = lazy(() => import("./components/page/Home"));
 
 function App() {
@@ -26,7 +29,7 @@ function App() {
 	}, []);
 	return (
 		<Provider store={appStore}>
-			<div className="">
+			<div className="min-h-[calc(100vh-97px)] bg-gradient-to-br to-gray-900 from-gray-950">
 				<ToastContainer
 					position="top-left"
 					autoClose={3000}
@@ -49,16 +52,12 @@ function App() {
 							<Routes>
 								<Route path="/login" element={<Login />} />
 								<Route path="/" element={<Home />} />
-								{/* <Route path="/about-us" element={<AboutUs />} />
-								<Route path="/contact" element={<Contact />} />
+								<Route path="/about-us" element={<AboutUs />} />
 								<Route
-									path="/privacy-policy"
-									element={<PrivacyPolicy />}
+									path="/contact-us"
+									element={<Contact />}
 								/>
-								<Route
-									path="/terms-of-service"
-									element={<TermsOfService />}
-								/> */}
+								<Route path="/planets" element={<Planets />} />
 								<Route path="*" element={<PageNotFound />} />
 							</Routes>
 						</Suspense>
